56901ae1b355395c32586e47ea019803a85d7f70
erickromo117 <rebyvodka11768@gmail.com>
2025-05-08 19:13:50 -0600

    update

--- cypress/e2e/cuadritos/cy342.cy.js
+++ cypress/e2e/cuadritos/cy342.cy.js
@@ -1,23 +1,23 @@
 describe('Publicaciones', () => {
     it('abre el tile de Publicaciones y comprueba el título', () => {
-      // 1) Visitar home y hacer login
+     
       cy.visit('https://www.lasallebajio.edu.mx/');
       cy.get('#login_link').click();
       cy.get('input[name="usuario"]').first().type('00075441');
       cy.get('input[name="pass"]').first().type('espartan117');
       cy.get('button[name="Entrar"]').first().click();
   
-      // 2) Clic en el tile de Publicaciones
+     
       cy.get('a[href="../publicaciones.php"]')
-        .first()                         // en caso de que haya más de uno
-        .invoke('removeAttr','target')   // para que no abra nueva pestaña
-        .scrollIntoView()                // baja hasta él
-        .click({ force: true });         // fuerza el click por si algo lo cubre
+        .first()                        
+        .invoke('removeAttr','target')  
+        .scrollIntoView()               
+        .click({ force: true });        
   
-      // 3) Comprobar que estamos en la URL correcta
+     
       cy.url().should('include', '/publicaciones.php');
   
-      // 4) Validar el <h1> que debe decir “Publicaciones”
+     
       cy.get('h1')
         .should('be.visible')
         .and('contain.text', 'Publicaciones');
